505ad3e6204144796bd2d828d3930ac0e060de70
Farah Nasreen <farahnasreen2000@gmail.com>
2022-03-21 23:44:10 +0530

    Add basic supports for adding multiple notes

--- index.js
+++ index.js
@@ -10,20 +10,49 @@ app.use(bodyParser.json());
 app.post("/api/insert", (req, res) => {
     console.log(req.body)
     var obj = req.body;
-    var content = obj.value;
-    console.log(content);
-    fs.writeFile('file.txt', content, (err) => {
-        if(err) throw err;
-        console.log('file saved');
-    })    
+    var note = obj.value;
+    console.log(note);
+   // fs.writeFile('file.txt', note, (err) => {
+   //     if(err) throw err;
+   //     console.log('file saved');
+   // })    
+   // fs.appendFile('file.txt',note, (err) => {
+   //     if(err){
+   //         throw err;
+   //     }else{
+   //         console.log('done!');
+   //     }
+   // })
+
+    fs.readFile('file.txt', 'utf8', (err, data) => {
+        if(data.length == 0){
+            console.log(data)
+            var notes = [];
+            notes.push(note);
+            var fileContent = JSON.stringify(notes);
+            fs.writeFile('file.txt', fileContent, (err) => {
+                if(err) throw err;
+                console.log('done');
+            })
+        }
+        else{
+            notes = JSON.parse(data);
+            notes.push(note);
+            var fileContent = JSON.stringify(notes);
+            fs.writeFile('file.txt', fileContent, (err) => {
+                if(err) throw err;
+                console.log('file saved');
+            })
+        }
+    })
 })
 
-app.get("/api/get",(req, res) => {
-    fs.readFile('file.txt', 'utf8', (err, data) => {
-        res.send(data)
-        console.log('file read', data)
-    }) 
-})
+//app.get("/api/get",(req, res) => {
+//    fs.readFile('file.txt', 'utf8', (err, data) => {
+//        res.send(data)
+//        console.log('file read', data)
+//    }) 
+//})
 
 app.listen(8080, () => {
     console.log('Server running')
