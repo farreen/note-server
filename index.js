d78518455fb4630b506f41b0536389e744423e76
Farah Nasreen <farahnasreen2000@gmail.com>
2022-04-25 23:03:00 +0530

    Add note update API

--- index.js
+++ index.js
@@ -6,30 +6,18 @@ const fs = require('fs');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const {v4: uuid} = require('uuid');
-app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.urlencoded({extended: true}));
 app.use(cors());
 app.use(bodyParser.json());
 
 app.post("/api/insert", (req, res) => {
-    console.log(req.body)
+    console.log(req.body);
     var note = {
         id: uuid(),
         title: req.body.title,
         content: req.body.content
     };
     console.log(note);
-   // fs.writeFile('file.txt', note, (err) => {
-   //     if(err) throw err;
-   //     console.log('file saved');
-   // })    
-   // fs.appendFile('file.txt',note, (err) => {
-   //     if(err){
-   //         throw err;
-   //     }else{
-   //         console.log('done!');
-   //     }
-   // })
-
     fs.readFile('file.txt', 'utf8', (err, data) => {
         var notes = [];
         if(data.length !== 0){
@@ -38,26 +26,67 @@ app.post("/api/insert", (req, res) => {
         notes.push(note);
         var fileContent = JSON.stringify(notes);
         fs.writeFile('file.txt', fileContent, (err) => {
-            if(err) throw err;
-            console.log('file saved');
+            if(err){
+                res.status(500);  
+                res.send();    
+            }else{
+                res.status(200);
+                res.send();  
+                console.log('file saved');
+            }
         })
     })
-})
+});
 
 app.get("/api/list", (req, res) => {
     fs.readFile('file.txt', 'utf8', (err, data) => {
-        res.send(data);
-        console.log('file read', data)
+        if(err) {
+            res.status(500);
+            res.send();
+        }else{
+            res.send(data);
+        }
     })
-})
+});
 
 //app.get("/api/get",(req, res) => {
 //    fs.readFile('file.txt', 'utf8', (err, data) => {
 //        res.send(data)
 //        console.log('file read', data)
-//    }) 
-//})
+//    })
+//});
+
+app.put('/api/update', (req, res) => {
+    const {id, title, content} = req.body;
+    fs.readFile('file.txt', 'utf8', (err, data) => {
+        var notes = JSON.parse(data);
+        console.log("file content", notes);
+        for(const obj of notes) {
+            console.log('object', obj);
+            if(obj.id === id) {
+                console.log('inside if block');
+                obj.title = title;
+                obj.content = content;    
+                var newContent = JSON.stringify(notes);
+                fs.writeFile('file.txt', newContent, (err) => {
+                    if(err) {
+                        res.status(500);
+                        res.send();
+                    }else{
+                        console.log('file updated');
+                        res.status(200);
+                        res.send();
+                    }
+                })
+                return;  
+            }
+        }
+        res.status(404);
+        res.send();
+        console.log('id not found');
+    })
+});
 
 app.listen(8080, () => {
-    console.log('Server running')
-})
+    console.log('Server running');
+});
