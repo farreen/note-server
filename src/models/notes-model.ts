eb3aa32106c43a98c4ba561b6720d6492cde6244
Farah Nasreen <farahnasreen2000@gmail.com>
2022-12-14 13:56:04 +0530

    Create schema for the notes

--- src/models/notes-model.ts
+++ src/models/notes-model.ts
@@ -5,13 +5,23 @@ type noteType = {
     title: string;
     content: string;
     tags: string;
+    date: string;
 }
+
 const noteSchema: Schema = new mongoose.Schema({
     title: { type: String, required: true },
-    content: {type: String, require: true},
-    tags: [{type: String, required: true}],
-
+    content: {type: String, require: true },
+    tags: [{ type: String, required: true }],
+    date: { type: String, default: new Date().toISOString() },
 });
+
+noteSchema.method('toJSON', function () {
+   const {_id, ...object } = this.toObject();
+   object.id = _id;
+   return object;
+});
+
+
 console.log("inside note-model-2");
 
 export default mongoose.model<noteType>("Note", noteSchema)
