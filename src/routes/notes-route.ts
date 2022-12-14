9e1edbda10b196443a499782e666b3c080fadcab
Farah Nasreen <farahnasreen2000@gmail.com>
2022-12-14 14:07:34 +0530

    Add route for createNotes

--- src/routes/notes-route.ts
+++ src/routes/notes-route.ts
@@ -2,6 +2,7 @@ import express  from "express";
 import controller from "../controllers/notes-controller"
 const router  = express.Router();
 
+router.post("/notes", controller.createNotes);
 router.get("/notes", controller.readNotes);
 
 console.log("notesRouter");
