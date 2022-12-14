95c6c0ecabae517744de2ea78c83afacf9692587
Farah Nasreen <farahnasreen2000@gmail.com>
2022-12-14 14:04:01 +0530

    Add createNotes function

--- src/controllers/notes-controller.ts
+++ src/controllers/notes-controller.ts
@@ -2,6 +2,22 @@ import express, {Request, Response} from "express";
 
 import NoteModel from "../models/notes-model";
 
+const createNotes = (req: Request, res: Response) => {
+    const {title, content, tags} = req.body;
+    console.log("req Body..", req.body);
+
+    if(title.length === 0 || content.length === 0 || title.length <= 4 || content.length <= 4 || tags.length == 0) {
+        res.status(400);
+        res.send("title and content should have atleast five character");
+    }else{
+        const newNote = new NoteModel({title, content, tags})
+        console.log("inside create", newNote);
+        newNote.save()
+        .then(() => res.status(200).send(newNote.id))
+        .catch((err: Error) => res.status(400).send(`Error: ${err}`))
+    }
+}
+
 const readNotes = (_: Request, res: Response) => {
     console.log("inside readNotes")
     NoteModel.find()
@@ -10,5 +26,5 @@ const readNotes = (_: Request, res: Response) => {
 }
 
 
-export default {readNotes}
+export default {readNotes, createNotes}
 
