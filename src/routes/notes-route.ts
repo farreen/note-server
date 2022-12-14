7aec7e0f1586312996db6894576815896332fe11
Farah Nasreen <farahnasreen2000@gmail.com>
2022-12-14 13:57:38 +0530

    Add route to get the list of notes

--- src/routes/notes-route.ts
+++ src/routes/notes-route.ts
@@ -2,11 +2,8 @@ import express  from "express";
 import controller from "../controllers/notes-controller"
 const router  = express.Router();
 
-router.post("/notes", controller.createNotes);
 router.get("/notes", controller.readNotes);
-console.log("notesRouter")
-//router.get("/get", controller.readNotes);
-//router.put("/update", controller.updateNotes);
-//router.delete("/delete", controller.deleteNotes);
+
+console.log("notesRouter");
 
 export default router;
